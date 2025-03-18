461e41d76eedb037fc16c93178a62f185cda4f30
117503445 <t117503445@gmail.com>
2025-03-18 14:49:01 +0000

    ci: 添加网络配置文件
    - 在构建镜像之前，复制 net.toml.example 为 net.toml
    - 这个修改确保了在 CI 环境中运行时有正确的网络配置
    
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- scripts/build-img-ci.sh
+++ scripts/build-img-ci.sh
@@ -2,5 +2,7 @@
 
 set -e
 
+cp ./assets/flake/nodes/nixos-test/net.toml.example ./assets/flake/nodes/nixos-test/net.toml
+
 docker build -t 117503445/nixos-playground-ci -f ./scripts/ci/Dockerfile .
 docker run --rm -v $(pwd):/workspace -v docker:/var/lib/docker --privileged --cap-add=NET_ADMIN --device /dev/net/tun 117503445/nixos-playground-ci
